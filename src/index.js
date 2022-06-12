62e3479a37a5c4a6f04dc7a541ab3adddc31ff91
ShubhangiBane <98038587+ShubhangiBane@users.noreply.github.com>
2022-06-12 13:07:35 +0530

    Completed All Checkpoint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'typeface-roboto';
-import registerServiceWorker from './registerServiceWorker';
-import Controller from './screens/Controller';
 
-ReactDOM.render(<Controller />, document.getElementById('root'));
-registerServiceWorker();
+import Home from './screens/home/Home'
+import Details from './screens/details/Details'
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import BookShow from './screens/bookshow/BookShow';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path={"/details"} element={<Details />} />
+        <Route exact path={"/bookshow"} element={<BookShow />} />
+      </Routes>
+    </BrowserRouter>,
+  </React.StrictMode>,
+  document.getElementById('root')
+);
